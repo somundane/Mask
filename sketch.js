85118963ec506fc20f80291f689ebe9cc27918c7
somundane <98dana@gmail.com>
2020-11-14 23:36:35 -0500

    EyePoints organized
    Given proper names for x and y for easy access later

--- sketch.js
+++ sketch.js
@@ -38,44 +38,94 @@ function draw() {
     // as the size of the mask.
         let d = dist(rEye.x, rEye.y, lEye.x, lEye.y);
         stroke(255);
-        line(rEye.x, rEye.y, lEye.x, lEye.y);
+        //line(rEye.x, rEye.y, lEye.x, lEye.y);
         fill(255, 0, 0);
-        let size = d * 0.17;
+        let size = d * 0.20;
 //        ellipse(rEye.x, rEye.y, size)
 //        ellipse(lEye.x, lEye.y, size)
         
-        //lower points
-        ellipse(rEye.x, rEye.y + size/2, 5)
-        ellipse(lEye.x, lEye.y + size/2, 5)
-        
-        //upper points
-        ellipse(rEye.x, rEye.y - size/2, 5)
-        ellipse(lEye.x, lEye.y - size/2, 5)
+//        //lower points
+//        ellipse(rEye.x, rEye.y + size/2, 5)
+//        ellipse(lEye.x, lEye.y + size/2, 5)
+//        
+//        //upper points
+//        ellipse(rEye.x, rEye.y - size/2, 5)
+//        ellipse(lEye.x, lEye.y - size/2, 5)
         
         let eyenoseR = dist(rEye.x, 0, nose.x, 0);
-        strokeWeight(2);
         let eyenoseL = dist(lEye.x, 0, nose.x, 0);
+        
+        let rEyeIn ={
+            x: rEye.x + size,
+            y: rEye.y
+        }
+        let rEyeOut = {
+            x: rEye.x - eyenoseR * 0.50,
+            y: rEye.y,
+            stillx: rEye.x - size * 1.25,
+            stilly: rEye.y
+        }
+        let rEyeUp = {
+            x: rEye.x,
+            y: rEye.y - size/2
+        }
+        let rEyeDown = {
+            x: rEye.x,
+            y: rEye.y + size/2
+        }
+        let lEyeIn = {
+            x: lEye.x - size,
+            y: lEye.y
+        }
+        let lEyeOut = {
+            x: lEye.x + eyenoseL * 0.50,
+            y: lEye.y,
+            stillx: lEye.x + size * 1.25,
+            stilly: lEye.y
+        }
+        let lEyeUp = {
+            x: lEye.x,
+            y: lEye.y - size/2
+        }
+        let lEyeDown = {
+            x: lEye.x,
+            y: lEye.y + size/2
+        }
+
         strokeWeight(2);
         //check which has less dist & apply
         if(eyenoseR < eyenoseL) {
-            //outer point
-            ellipse(rEye.x - eyenoseR * 0.50, rEye.y, 5)
-            ellipse(lEye.x + size * 1.25, lEye.y, 5)
+            //right
+            line(rEyeOut.x, rEyeOut.y, rEyeDown.x, rEyeDown.y);
+            line(rEyeOut.x, rEyeOut.y, rEyeUp.x, rEyeUp.y);
+            
+            //left
+            line(lEyeOut.stillx, lEyeOut.stilly, lEyeDown.x, lEyeDown.y);
+            line(lEyeOut.stillx, lEyeOut.stilly, lEyeUp.x, lEyeUp.y);
         }
         else {
-            //outer point
-            ellipse(rEye.x - size * 1.25, rEye.y, 5)
-            ellipse(lEye.x + eyenoseL * 0.50, lEye.y, 5)
+            //right
+            line(rEyeOut.stillx, rEyeOut.stilly, rEyeDown.x, rEyeDown.y);
+            line(rEyeOut.stillx, rEyeOut.stilly, rEyeUp.x, rEyeUp.y);
+            
+            //left
+            line(lEyeOut.x, lEyeOut.y, lEyeUp.x, lEyeUp.y);
+            line(lEyeOut.x, lEyeOut.y, lEyeDown.x, lEyeDown.y);
         }
-        let outReye = size * 1.25;
+        //let outReye = size * 1.25;
         
+        //inner lines
+        //right
+        line(rEyeIn.x, rEyeIn.y, rEyeDown.x, rEyeDown.y);
+        line(rEyeIn.x, rEyeIn.y, rEyeUp.x, rEyeUp.y);
+            
+        //left
+        line(lEyeIn.x, lEyeIn.y, lEyeUp.x, lEyeUp.y);
+        line(lEyeIn.x, lEyeIn.y, lEyeDown.x, lEyeDown.y);
         
         //inner points
-        ellipse(rEye.x + size, rEye.y, 5)
-        ellipse(lEye.x - size, lEye.y, 5)
-
-        
-        line()
+//        ellipse(rEye.x + size, rEye.y, 5)
+//        ellipse(lEye.x - size, lEye.y, 5)
         
 //    fill(255, 0, 0, 50);
 //    ellipse(pose.nose.x, pose.nose.y, d, d*1.25);
@@ -96,7 +146,7 @@ function draw() {
 function confidence(obj) {
     
 }
-function drawREye(x, y) {
+function drawEyes(x, y) {
     translate(x, y);
     
 }
